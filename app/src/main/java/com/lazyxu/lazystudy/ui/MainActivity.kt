b81d4049bfee3b0030d2e7e536f7c5515f27da49
xuyexiang <605568743@qq.com>
2024-08-08 15:40:04 +0800

    save2

--- app/src/main/java/com/lazyxu/lazystudy/ui/MainActivity.kt
+++ app/src/main/java/com/lazyxu/lazystudy/ui/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.lazyxu.lazystudy.ui
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
@@ -19,14 +20,6 @@ import kotlin.system.exitProcess
 
 @Route(path = ARouterPath.MAIN)
 class MainActivity : BaseVbActivity<ActivityMainBinding>() {
-
-    private var mFragments: MutableList<Fragment> =
-        mutableListOf(
-            HomeFragment(),
-            CategoryFragment(),
-            ARouterHelper.getFragment(ARouterPath.Mine.MAIN)
-        )
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (savedInstanceState == null) {
@@ -38,43 +31,45 @@ class MainActivity : BaseVbActivity<ActivityMainBinding>() {
         mViewBinding.bnvMain.itemIconTintList = null//设置tab图标，注意使用 app:itemIconTint="@null"设置无效
         mViewBinding.bnvMain.setOnItemSelectedListener {
             when (it.itemId) {
-                R.id.menu_home -> {
-                    setFragmentPosition(0)
-                }
-
-                R.id.menu_type -> {
-                    setFragmentPosition(1)
-                }
-
-                R.id.menu_mine -> {
-                    setFragmentPosition(2)
-                }
-
+                R.id.menu_home -> setFragmentPosition(0)
+                R.id.menu_type -> setFragmentPosition(1)
+                R.id.menu_mine -> setFragmentPosition(2)
                 else -> {}
             }
             true
         }
     }
 
+    private var mFragments: MutableList<Fragment> =
+        mutableListOf(
+            HomeFragment(),
+            CategoryFragment(),
+            ARouterHelper.getFragment(ARouterPath.Mine.MAIN)
+        )
     private var lastIndex = -1
     private fun setFragmentPosition(position: Int) {
         try {
             if (lastIndex == position) return
-            val ft = supportFragmentManager.beginTransaction()
+            val fragmentManager = supportFragmentManager
+            val ft = fragmentManager.beginTransaction()
             val currentFragment = mFragments[position]
-            mFragments.filter { it != currentFragment && it.isAdded }.forEach {
-                ft.hide(it)
-                ft.setMaxLifecycle(it, Lifecycle.State.STARTED)
-            }
-            lastIndex = position
+            //这里要用fragmentManager.fragments.filter，不能用mFragments.filter
+            fragmentManager.fragments.filter { it != currentFragment && it.isAdded }
+                .forEach {
+                    Log.d("FragmentManager","hide=${it}")
+                    ft.hide(it)
+                    ft.setMaxLifecycle(it, Lifecycle.State.STARTED)
+                }
             if (!currentFragment.isAdded) {
-                ft.remove(currentFragment)
-                ft.add(R.id.fragment_main, currentFragment)//,"mainmenu${position}"
+                Log.d("FragmentManager","add=${currentFragment}")
+                ft.add(R.id.fragment_main, currentFragment)
             } else {
+                Log.d("FragmentManager","add=${currentFragment}")
                 ft.show(currentFragment)
             }
             ft.setMaxLifecycle(currentFragment, Lifecycle.State.RESUMED)
             ft.commitAllowingStateLoss()
+            lastIndex = position
         } catch (e: Exception) {
             LogUtils.d("setFragmentPosition=${e}")
         }
